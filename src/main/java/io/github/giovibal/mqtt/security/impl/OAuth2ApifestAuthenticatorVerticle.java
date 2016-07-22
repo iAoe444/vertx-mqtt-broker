b0f9f0dcd503ecc4344c10f86a1f8f125f13885e
Giovanni Baleani <giovibal@gmail.com>
2016-07-22 12:06:06 +0200

    Bugfix apifest auth

--- src/main/java/io/github/giovibal/mqtt/security/impl/OAuth2ApifestAuthenticatorVerticle.java
+++ src/main/java/io/github/giovibal/mqtt/security/impl/OAuth2ApifestAuthenticatorVerticle.java
@@ -156,7 +156,7 @@ public class OAuth2ApifestAuthenticatorVerticle extends AuthenticatorVerticle {
                 if(username.contains("@")) {
                     String data = "grant_type=password&username=" + username + "&password=" + password + "&scope=sp&client_id=" + app_key + "&client_secret=" + app_secret+"";
                     loginReq.putHeader("Content-Type", "application/x-www-form-urlencoded");
-                    loginReq.putHeader("content-length", "");
+//                    loginReq.putHeader("content-length", "");
                     loginReq.end(data, "UTF-8");
                 }
                 else {
