df94fbec0a3a0873175f707267089ffbc4ff554f
Giovanni Baleani <giovibal@gmail.com>
2017-04-21 16:12:56 +0200

    Fixes #15

--- scripts/test_post.sh
+++ scripts/test_post.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 curl -XPOST \
     -d '{"test","qos 0"}' \
     -H "tenant: tenant.local" \
@@ -10,7 +9,6 @@ curl -XPOST \
     -d '{"test","qos 1"}' \
     -H "tenant: tenant.local" \
     "http://localhost:2883/pubsub/publish?topic=/tenant.local/channel/1&qos=LEAST_ONE"
-
 
 curl -XPOST \
     -d '{"test","qos 2"}' \
